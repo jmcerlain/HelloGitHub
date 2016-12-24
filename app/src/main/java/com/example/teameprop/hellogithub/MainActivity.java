12b591f850e78803228258bd5a40e215395f6414
Teameprop <jmcbay66@fastmail.com>
2016-12-24 00:03:50 -0500

    Completed adding the button to the main screen

--- app/src/main/java/com/example/teameprop/hellogithub/MainActivity.java
+++ app/src/main/java/com/example/teameprop/hellogithub/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.teameprop.hellogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.content.Context;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void btnClicked(View v){
+        Context content = getApplicationContext();
+        CharSequence text = "Button Clicked!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(content, text, duration);
+        toast.show();
     }
 }
